2ca50659c34fa8171c55f1141a3a7d223a5bef1a
allen.porter <allen.porter@58fa8ae0-6a36-0410-a7a1-b30e0234c60e>
2007-08-13 04:25:28 +0000

    Removed objc_msgSend_fpret hack and updated README (roadmap) a tad.

--- branches/vt100-iterm/main.m
+++ branches/vt100-iterm/main.m
@@ -2,10 +2,6 @@
 #import <UIKit/UIKit.h>
 #import "MobileTerminal.h"
 
-// TODO: This is a workaround to fix a link error; not sure what the
-// consequences will be.
-void objc_msgSend_fpret(int x) { return; }
-
 int main(int argc, char **argv)
 {
     [[NSAutoreleasePool alloc] init];
