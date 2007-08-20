adc38b3365572abf07972900bde175c02ffffd49
vaumnou <vaumnou@58fa8ae0-6a36-0410-a7a1-b30e0234c60e>
2007-08-20 23:30:40 +0000

    Made the pie menu animation cooler via alpha fade.  The slow version for the initial fadeout uses only alpha fade, no scale. '

--- branches/vt100/PieView.m
+++ branches/vt100/PieView.m
@@ -2,6 +2,7 @@
 #import <UIKit/UIView-Geometry.h>
 #import <UIKit/UIAnimator.h>
 #import <UIKit/UITransformAnimation.h>
+#import <UIKit/UIAlphaAnimation.h>
 #import <UIKit/UIView-Rendering.h>
 
 @implementation PieView
@@ -20,11 +21,17 @@
     if (!_visible) {
         _visible = YES;
         [self setTransform:CGAffineTransformMake(0.01f,0,0,0.01f,0,480)];
+        [self setAlpha: 0.0f];
         //[self setFrame:hiddenFrame];
         UITransformAnimation *scaleAnim = [[UITransformAnimation alloc] initWithTarget: self];
         [scaleAnim setStartTransform: CGAffineTransformMake(0.01f,0,0,0.01f,0,0)];
         [scaleAnim setEndTransform: CGAffineTransformMake(1,0,0,1,0,0)];
-        [[[UIAnimator alloc] init] addAnimation:scaleAnim withDuration:0.15f start:YES]; 
+        UIAlphaAnimation *alphaAnim = [[UIAlphaAnimation alloc] initWithTarget: self];
+        [alphaAnim setStartAlpha: 0.0f];
+        [alphaAnim setEndAlpha: 0.9f];
+        UIAnimator *anim = [[UIAnimator alloc] init];
+        [anim addAnimation:scaleAnim withDuration:0.15f start:YES]; 
+        [anim addAnimation:alphaAnim withDuration:0.15f start:YES];
         //[self setTransform:CGAffineTransformMake(1,0,0,1,0,0)];
         //[self setFrame:visibleFrame];
     }
@@ -39,7 +46,13 @@
         UITransformAnimation *scaleAnim = [[UITransformAnimation alloc] initWithTarget: self];
         [scaleAnim setStartTransform: CGAffineTransformMake(1,0,0,1,0,0)];
         [scaleAnim setEndTransform: CGAffineTransformMake(0.01f,0,0,0.01f,0,0)];
-        [[[UIAnimator alloc] init] addAnimation:scaleAnim withDuration:(slow ? 1.0f : 0.25f) start:YES]; 
+        UIAlphaAnimation *alphaAnim = [[UIAlphaAnimation alloc] initWithTarget: self];
+        [alphaAnim setStartAlpha: 0.9f];
+        [alphaAnim setEndAlpha: 0.0f];
+        float duration = slow ? 1.0f : 0.25f;
+        UIAnimator *anim = [[UIAnimator alloc] init];
+        if (!slow) [anim addAnimation:scaleAnim withDuration:duration start:YES]; 
+        [anim addAnimation:alphaAnim withDuration:duration start:YES];
         _visible = NO;
     }
 }
