93c9e48641c4a3df3877df4189318b4f01b485f5
saurik <saurik@58fa8ae0-6a36-0410-a7a1-b30e0234c60e>
2008-07-31 06:59:18 +0000

    Splitting off the trunk for the 2.x Telesphoreo branch.

---
+++ branches/saurik/Sources/UI/Keyboard.h
@@ -0,0 +1,20 @@
+// ShellKeyboard.h
+#import <UIKit/UIKit.h>
+#import <UIKit/UITextView.h>
+
+@protocol KeyboardInputProtocol
+- (void)handleKeyPress:(unichar)c;
+@end
+
+@interface ShellKeyboard : UIKeyboard<KeyboardInputProtocol>
+{
+  id inputDelegate;
+  UITextView* inputView;
+}
+
+- (id)initWithFrame:(CGRect)frame;
+- (UITextView*)inputView;
+- (void)setInputDelegate:(id)delegate;
+- (void)handleKeyPress:(unichar)c;
+
+@end
